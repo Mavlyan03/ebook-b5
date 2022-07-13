bd3a77cc186fb28ff1d4751e0616d883930df1ba
Aichurok <tasnim0800@gmail.com>
2022-07-14 00:40:56 +0600

    added entities

---
+++ src/main/java/kg/eBook/ebookb5/enums/BookStatus.java
@@ -0,0 +1,11 @@
+package kg.eBook.ebookb5.enums;
+
+public enum BookStatus {
+
+    IN_PROCESSING("В обработке"),
+    ACCEPTED("Принятый"),
+    REJECTED("Отклоненный");
+
+    BookStatus(String status) {
+    }
+}

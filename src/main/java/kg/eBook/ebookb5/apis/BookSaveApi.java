5db024d3079ba11deb15a649ee3dab5c85fdccdd
Ahmed Timur <mathfun65@gmail.com>
2022-08-01 11:40:25 +0600

    feature/bookCRUD

---
+++ src/main/java/kg/eBook/ebookb5/apis/BookSaveApi.java
@@ -0,0 +1,16 @@
+package kg.eBook.ebookb5.apis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/book")
+@CrossOrigin
+public class BookSaveApi {
+
+
+
+}

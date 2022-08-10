9cba33d9eddc22b0b2b6b74d4ea7727d3d2b222f
beksultan <beksultancs@gmail.com>
2022-08-10 18:38:18 +0600

    added default constructor to UserREgisterRequest.java and VendorRegisterRequest.java

--- src/main/java/kg/eBook/ebookb5/dto/requests/UserRegisterRequest.java
+++ src/main/java/kg/eBook/ebookb5/dto/requests/UserRegisterRequest.java
@@ -3,15 +3,17 @@ package kg.eBook.ebookb5.dto.requests;
 import kg.eBook.ebookb5.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserRegisterRequest {
 
     private String firstName;
     private String email;
     private String password;
-    private Role role;
+
 }

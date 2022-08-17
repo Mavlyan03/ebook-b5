1895f0117778e5dde90035eeb0c5fd76eb6da94c
Altynbek <altynbekzhumadiluulu@gmail.com>
2022-08-17 22:01:50 +0600

    description

--- src/main/java/kg/eBook/ebookb5/apis/NotificationApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/NotificationApi.java
@@ -1,5 +1,6 @@
 package kg.eBook.ebookb5.apis;
 
+import io.swagger.v3.oas.annotations.Operation;
 import kg.eBook.ebookb5.dto.responses.NotificationFindByIdResponse;
 import kg.eBook.ebookb5.dto.responses.NotificationResponse;
 import kg.eBook.ebookb5.dto.responses.SimpleResponse;
@@ -21,30 +22,35 @@ public class NotificationApi {
 
     @PostMapping("/books/{bookId}/accepted")
     @PreAuthorize("hasAuthority('ADMIN')")
+    @Operation(summary = "accepted a book", description = "accepted a book with by id")
     public SimpleResponse acceptedBook(@PathVariable Long bookId) {
         return notificationService.acceptedBook(bookId);
     }
 
     @PostMapping("/books/{bookId}/rejected")
     @PreAuthorize("hasAuthority('ADMIN')")
+    @Operation(summary = "rejected a book", description = "rejected a book with by id")
     public SimpleResponse rejectedBook(@PathVariable Long bookId, @RequestParam String description) {
         return notificationService.rejectedBook(bookId, description);
     }
 
     @GetMapping
     @PreAuthorize("hasAuthority('VENDOR')")
+    @Operation(summary = "find all notifications with by authentication")
     public List<NotificationResponse> findAllNotificationsByVendor(Authentication authentication) {
         return notificationService.findAllNotificationsByVendor(authentication);
     }
 
     @GetMapping("/{notificationId}")
     @PreAuthorize("hasAuthority('VENDOR')")
+    @Operation(summary = "find by notification with by id")
     public NotificationFindByIdResponse findByNotificationId(@PathVariable Long notificationId) {
         return notificationService.findByNotificationId(notificationId);
     }
 
     @PutMapping("/markAsRead")
     @PreAuthorize("hasAuthority('VENDOR')")
+    @Operation(summary = "mark as read", description = "mark as read return notifications list")
     public List<NotificationResponse> markAsRead(Authentication authentication) {
         return notificationService.markAsRead(authentication);
     }

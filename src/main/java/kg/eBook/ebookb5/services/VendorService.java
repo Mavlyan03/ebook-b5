dd04b02cb39f0caa9e1d0bac482595c65f03441d
Altynbek <altynbekzhumadiluulu@gmail.com>
2022-09-03 19:18:19 +0600

    refactor vendor book add pagination

--- src/main/java/kg/eBook/ebookb5/services/VendorService.java
+++ src/main/java/kg/eBook/ebookb5/services/VendorService.java
@@ -2,10 +2,10 @@ package kg.eBook.ebookb5.services;
 
 import kg.eBook.ebookb5.dto.requests.VendorProfileRequest;
 import kg.eBook.ebookb5.dto.requests.VendorRegisterRequest;
-import kg.eBook.ebookb5.dto.responses.ABookVendorResponse;
 import kg.eBook.ebookb5.dto.responses.JwtResponse;
 import kg.eBook.ebookb5.dto.responses.SimpleResponse;
 import kg.eBook.ebookb5.dto.responses.VendorResponse;
+import kg.eBook.ebookb5.dto.responses.ABookVendorResponse;
 import kg.eBook.ebookb5.enums.AboutBooks;
 import kg.eBook.ebookb5.enums.BookStatus;
 import kg.eBook.ebookb5.enums.Role;
@@ -13,13 +13,17 @@ import kg.eBook.ebookb5.exceptions.AlreadyExistException;
 import kg.eBook.ebookb5.exceptions.NotFoundException;
 import kg.eBook.ebookb5.exceptions.WrongPasswordException;
 import kg.eBook.ebookb5.models.Book;
-import kg.eBook.ebookb5.models.Notification;
 import kg.eBook.ebookb5.models.User;
+import kg.eBook.ebookb5.repositories.BookRepository;
 import kg.eBook.ebookb5.repositories.NotificationRepository;
 import kg.eBook.ebookb5.repositories.PurchasedUserBooksRepository;
 import kg.eBook.ebookb5.repositories.UserRepository;
 import kg.eBook.ebookb5.security.JWT.JWTUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -29,7 +33,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static kg.eBook.ebookb5.dto.responses.ABookVendorResponse.viewBooks;
 import static kg.eBook.ebookb5.dto.responses.VendorResponse.viewVendors;
 
 @Service
@@ -42,6 +45,7 @@ public class VendorService {
     private final PurchasedUserBooksRepository purchasedUserBooksRepository;
     private final NotificationRepository notificationRepository;
     private final JWTUtil jwtUtil;
+    private final BookRepository bookRepository;
 
     public JwtResponse registerVendor(VendorRegisterRequest vendorRegisterRequest) {
 
@@ -117,87 +121,115 @@ public class VendorService {
         return viewVendors(personRepository.findAllVendors());
     }
 
-    public List<ABookVendorResponse> findABookVendor(Long vendorId, AboutBooks aboutBooks) {
+    public Page<ABookVendorResponse> findABookVendor(Long vendorId, AboutBooks aboutBooks, int page, int size) {
+
         User vendor = personRepository.findById(vendorId).orElseThrow(
                 () -> new NotFoundException("Пользователь не найдено"));
+        Pageable pageable = PageRequest.of(page - 1, size);
         switch (aboutBooks) {
             case ALL:
-                return viewBooks(vendor.getBooks());
+                List<ABookVendorResponse> bookVendorResponses = ABookVendorResponse.viewBooks(vendor.getBooks());
+                return new PageImpl<>(bookVendorResponses.subList(start(pageable, bookVendorResponses),
+                        end(pageable, bookVendorResponses)), pageable, bookVendorResponses.size());
             case REJECTED:
-                return viewBooks(booksRejected(vendor.getBooks()));
+                return new PageImpl<>(booksRejected(vendor.getBooks()).subList(
+                        start(pageable, booksRejected(vendor.getBooks())),
+                        end(pageable, booksRejected(vendor.getBooks()))),
+                        pageable, booksRejected(vendor.getBooks()).size());
             case IN_THE_PROCESS:
-                return viewBooks(booksInTheProcess(vendor.getBooks()));
+                return new PageImpl<>(booksInTheProcess(vendor.getBooks()).subList(
+                        start(pageable, booksInTheProcess(vendor.getBooks())),
+                        end(pageable, booksInTheProcess(vendor.getBooks()))),
+                        pageable, booksInTheProcess(vendor.getBooks()).size());
             case WITH_DISCOUNTS:
-                return viewBooks(booksWithDiscounts(vendor.getBooks()));
+                return new PageImpl<>(booksWithDiscounts(vendor.getBooks()).subList(
+                        start(pageable, booksWithDiscounts(vendor.getBooks())),
+                        end(pageable, booksWithDiscounts(vendor.getBooks()))),
+                        pageable, booksWithDiscounts(vendor.getBooks()).size());
             case IN_THE_BASKET:
-                return viewBooks(booksInTheBasket(vendor.getBooks()));
+                return new PageImpl<>(booksInTheBasket(vendor.getBooks()).subList(
+                        start(pageable, booksInTheBasket(vendor.getBooks())),
+                        end(pageable, booksInTheBasket(vendor.getBooks()))),
+                        pageable, booksInTheBasket(vendor.getBooks()).size());
             case FAVORITES:
-                return viewBooks(booksFavorites(vendor.getBooks()));
+                return new PageImpl<>(booksFavorites(vendor.getBooks()).subList(
+                        start(pageable, booksFavorites(vendor.getBooks())),
+                        end(pageable, booksFavorites(vendor.getBooks()))),
+                        pageable, booksFavorites(vendor.getBooks()).size());
             case SOLD_OUT:
-                return viewBooks(booksSoldOut(vendor.getBooks()));
+                return new PageImpl<>(booksSoldOut(vendor.getBooks()).subList(
+                        start(pageable, booksSoldOut(vendor.getBooks())),
+                        end(pageable, booksSoldOut(vendor.getBooks()))),
+                        pageable, booksSoldOut(vendor.getBooks()).size());
             default:
                 return null;
         }
     }
 
-    private List<Book> booksRejected(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private int start(Pageable pageable, List<ABookVendorResponse> bookVendorResponses) {
+        return Math.min((int) pageable.getOffset(), bookVendorResponses.size());
+    }
+
+    private int end(Pageable pageable, List<ABookVendorResponse> bookVendorResponses) {
+        return Math.min((start(pageable, bookVendorResponses) + pageable.getPageSize()),
+                bookVendorResponses.size());
+    }
+
+    private List<ABookVendorResponse> booksRejected(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (book.getBookStatus().equals(BookStatus.REJECTED)) {
-                Notification notification = notificationRepository.findByBookId(book.getId()).orElseThrow(
-                        () -> new NotFoundException("Уведомление не найдено"));
-                book.setPublishedDate(notification.getCreatedAt());
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;
     }
 
-    private List<Book> booksInTheProcess(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private List<ABookVendorResponse> booksInTheProcess(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (book.getBookStatus().equals(BookStatus.IN_PROCESSING)) {
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;
     }
 
-    private List<Book> booksWithDiscounts(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private List<ABookVendorResponse> booksWithDiscounts(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (book.getDiscount() > 0) {
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;
     }
 
-    private List<Book> booksInTheBasket(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private List<ABookVendorResponse> booksInTheBasket(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (book.getBookBasket().size() > 0) {
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;
     }
 
-    private List<Book> booksFavorites(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private List<ABookVendorResponse> booksFavorites(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (book.getLikes().size() > 0) {
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;
     }
 
-    private List<Book> booksSoldOut(List<Book> books) {
-        List<Book> bookList = new ArrayList<>();
+    private List<ABookVendorResponse> booksSoldOut(List<Book> books) {
+        List<ABookVendorResponse> bookList = new ArrayList<>();
         for (Book book : books) {
             if (purchasedUserBooksRepository.existsPurchasedUserBooksByBookId(book.getId())) {
-                bookList.add(book);
+                bookList.add(new ABookVendorResponse(book));
             }
         }
         return bookList;

5807f2799940c2829634c3d193526a7077be8296
Azat <ibraevazati@gmail.com>
2022-09-12 18:50:39 +0300

    refactor operation history

--- src/main/java/kg/eBook/ebookb5/apis/UserApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/UserApi.java
@@ -18,7 +18,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -71,3 +70,4 @@ public class UserApi {
         return bookService.mainPageResponse();
     }
 }
+

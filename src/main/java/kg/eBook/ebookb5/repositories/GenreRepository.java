dc2401b76c742362bd2042fe3d95821ff4dcc9a0
Aichurok <tasnim0800@gmail.com>
2022-09-19 19:52:17 +0600

    refactor/sortBooksByVendor 1

--- src/main/java/kg/eBook/ebookb5/repositories/GenreRepository.java
+++ src/main/java/kg/eBook/ebookb5/repositories/GenreRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-    @Query("select count(b) from Book b where b.genre.id = :id and b.bookStatus = 'ACCEPTED'")
+    @Query("select count(b) from Book b where b.genre.id = :id")
     Optional<Long> quantityOfBook(Long id);
 
     @Query("select count(b) from Book b where b.genre.id = :id and b.bookStatus = :bookStatus")

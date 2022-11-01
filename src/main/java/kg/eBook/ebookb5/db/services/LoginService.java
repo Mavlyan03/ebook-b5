a27a30bc75f884d6ee5cbee468407f3adaed15c9
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 15:41:28 +0600

    refactor LoginService

--- src/main/java/kg/eBook/ebookb5/db/services/LoginService.java
+++ src/main/java/kg/eBook/ebookb5/db/services/LoginService.java
@@ -22,21 +22,15 @@ public class LoginService {
     private final PasswordEncoder passwordEncoder;
 
     public JwtResponse authenticate(LoginRequest loginRequest) {
-
         User user = personRepository.findByEmail(loginRequest.getEmail())
                 .orElseThrow(() -> new NotFoundException(
-                        "Пользователь с почтой " + loginRequest.getEmail() + " не найден"
-                ));
-
+                        "Пользователь с почтой " + loginRequest.getEmail() + " не найден"));
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            throw new WrongPasswordException(
-                    "Неверный пароль"
-            );
+            throw new WrongPasswordException("Неверный пароль");
         }
 
         String token = jwtUtil.generateToken(user.getEmail());
-
         return new JwtResponse(
                 user.getId(),
                 token,
@@ -44,4 +38,5 @@ public class LoginService {
                 user.getFirstName()
         );
     }
+
 }

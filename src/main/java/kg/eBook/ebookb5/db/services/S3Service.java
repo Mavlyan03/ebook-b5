e476e8ed1c2e9dc57336548280144150f53c53d0
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 16:13:38 +0600

    refactor S3Service

--- src/main/java/kg/eBook/ebookb5/db/services/S3Service.java
+++ src/main/java/kg/eBook/ebookb5/db/services/S3Service.java
@@ -1,6 +1,7 @@
 package kg.eBook.ebookb5.db.services;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
@@ -17,26 +17,21 @@ import java.util.Map;
 
 @Slf4j
 @Service
-@Getter@Setter
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class S3Service {
 
     private final S3Client s3;
+
     @Value("${aws.bucket.name}")
     private String bucketName;
 
     @Value("${aws.bucket.path}")
     private String bucketPath;
 
-    public S3Service(S3Client s3) {
-        this.s3 = s3;
-    }
-
-    // upload
     public Map<String, String> upload(MultipartFile file) throws IOException {
-
-        log.info("Uploading file ...");
         String key = System.currentTimeMillis() + file.getOriginalFilename();
-
         PutObjectRequest por = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
@@ -44,39 +39,20 @@ public class S3Service {
                         "image/jpeg; image/png; image/svg; image/webp; image/gif; image/apng;" +
                         "application/pdf")
                 .build();
-
         s3.putObject(por, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-
-        log.info("Upload complete.");
-
-        return Map.of(
-                "link", bucketPath + key
-        );
-
+        return Map.of("link", bucketPath + key);
     }
 
-    // delete
     public Map<String, String> delete(String fileLink) {
-
-        log.info("Deleting file...");
-
         try {
-
             String key = fileLink.substring(bucketPath.length());
-
-            log.warn("Deleting object: {}", key);
-
             s3.deleteObject(dor -> dor.bucket(bucketName).key(key).build());
-
         } catch (S3Exception e) {
             throw new IllegalStateException(e.awsErrorDetails().errorMessage());
         } catch (Exception e) {
             throw new IllegalStateException(e.getMessage());
         }
-
-        return Map.of(
-                "message", fileLink + " has been deleted."
-        );
+        return Map.of("message", fileLink + " has been deleted.");
     }
 
 }

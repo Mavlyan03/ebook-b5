03b6aa0e21ff7ac61600b4ff672dbdf8e2f96627
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 13:53:31 +0600

    refactor VendorApi

--- src/main/java/kg/eBook/ebookb5/apis/VendorApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/VendorApi.java
@@ -1,6 +1,7 @@
 package kg.eBook.ebookb5.apis;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.eBook.ebookb5.dto.requests.VendorProfileRequest;
 import kg.eBook.ebookb5.dto.responses.SimpleResponse;
 import kg.eBook.ebookb5.dto.responses.VendorResponse;
@@ -13,7 +14,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -21,44 +31,46 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @RequestMapping("api/vendors")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Vendor API", description = "Vendor endpoints for Vendor & Admin")
 public class VendorApi {
 
     private final VendorService vendorService;
 
-    @PutMapping("/vendor")
+    @Operation(summary = "Vendor profile update", description = "Vendor profile update")
     @PreAuthorize("hasAuthority('VENDOR')")
-    @Operation(summary = "update by vendor")
+    @PutMapping("profile")
     public VendorResponse update(Authentication authentication,
                                  @RequestBody @Valid VendorProfileRequest vendorProfileRequest,
                                  BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             throw new WrongEmailException("Неправильный адрес электронной почты");
         }
         return vendorService.update(authentication, vendorProfileRequest);
     }
 
-    @GetMapping("/{vendorId}")
-    @Operation(summary = "find by vendor with id")
+    @Operation(summary = "Get vendor by id", description = "Get vendor profile with id by Vandor & Admin")
     @PreAuthorize("hasAnyAuthority('VENDOR', 'ADMIN')")
+    @GetMapping("{vendorId}")
     public VendorResponse findByVendor(@PathVariable Long vendorId) {
         return vendorService.findByVendor(vendorId);
     }
 
-    @DeleteMapping("/{vendorId}")
+    @Operation(summary = "Delete vendor by id", description = "Removing vendor profile with id by Vendor & Admin ")
     @PreAuthorize("hasAnyAuthority('VENDOR', 'ADMIN')")
-    @Operation(summary = "delete by vendor with vendor id")
+    @DeleteMapping("{vendorId}")
     public SimpleResponse delete(@PathVariable Long vendorId) {
         return vendorService.deleteByVendorId(vendorId);
     }
 
-    @GetMapping("/{vendorId}/books")
-    @PreAuthorize("hasAnyAuthority('VENDOR', 'ADMIN')")
-    @Operation(summary = "find all books with vendor id", description = "can be obtained using vendor id " +
+    @Operation(summary = "Get all books by vendor", description = "Can be obtained using vendor id " +
             "all books, in favorites, in the basket, sold out with discounts in processing and rejected")
+    @PreAuthorize("hasAnyAuthority('VENDOR', 'ADMIN')")
+    @GetMapping("{vendorId}/books")
     public Page<ABookVendorResponse> findABookVendor(@PathVariable Long vendorId,
                                                      @RequestParam AboutBooks aboutBooks,
                                                      @RequestParam(required = false, defaultValue = "1") int page,
                                                      @RequestParam(required = false, defaultValue = "16") int size) {
         return vendorService.findABookVendor(vendorId, aboutBooks, page, size);
     }
+
 }

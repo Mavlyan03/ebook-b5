bbe243dfe09677129860e7416ad7d1ecee64ab70
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 15:33:15 +0600

    refactor AudioBookService

--- src/main/java/kg/eBook/ebookb5/db/services/book/AudioBookService.java
+++ src/main/java/kg/eBook/ebookb5/db/services/book/AudioBookService.java
@@ -34,21 +34,15 @@ public class AudioBookService {
     public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public BookResponse saveAudioBook(Authentication authentication, AudioBookSaveRequest audioBook) {
-
         checkIfBookExists(audioBook);
-
         Book book = new Book(audioBook);
-
         book.setBookType(AUDIO_BOOK);
-
         book.setGenre(genreRepository.findById(audioBook.getGenreId()).orElseThrow(() -> new NotFoundException(
-                "Жанр с ID: " + audioBook.getGenreId() + " не был найден"
-        )));
+                "Жанр с ID: " + audioBook.getGenreId() + " не был найден")));
 
         User user = userRepository.findByEmail(authentication.getName()).get();
         book.setOwner(user);
         user.setBook(book);
-
         Book savedBook = bookRepository.save(book);
 
         log.info("Audio book successfully saved");
@@ -61,9 +55,7 @@ public class AudioBookService {
     }
 
     private void checkIfBookExists(AudioBookSaveRequest audioBook) {
-
         Book book = bookRepository.findByName(audioBook.getName()).orElse(null);
-
         if (book != null) {
             if (book.getLanguage().equals(audioBook.getLanguage()) && book.getBookType().equals(AUDIO_BOOK)) {
                 log.error("This book is already in the database");
@@ -73,7 +65,6 @@ public class AudioBookService {
     }
 
     public void updateBook(Authentication authentication, Long bookId, AudioBookSaveRequest audioBook) {
-
         User user = userRepository.findByEmail(authentication.getName()).get();
         Book book = bookRepository.findById(bookId).orElseThrow(
                 () -> new NotFoundException("Книга с  ID: " + bookId + " не найдена"));
@@ -81,8 +72,8 @@ public class AudioBookService {
         if (book.getBookType().equals(AUDIO_BOOK)) {
             book.setName(audioBook.getName());
             book.setGenre(genreRepository.findById(audioBook.getGenreId()).orElseThrow(() -> new NotFoundException(
-                    "Жанр с  ID: " + audioBook.getGenreId() + " не найден"
-            )));
+                    "Жанр с  ID: " + audioBook.getGenreId() + " не найден")));
+
             book.setPrice(audioBook.getPrice());
             book.setAuthor(audioBook.getAuthor());
             book.setDescription(audioBook.getDescription());
@@ -105,4 +96,5 @@ public class AudioBookService {
         } else
             throw new IllegalStateException("Вы не можете редактировать эту книгу!");
     }
+
 }

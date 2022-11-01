557d25aab5d312ed9b97edaa6a8a527ad4c7fd46
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 15:20:16 +0600

    refactor MailingList entity

--- src/main/java/kg/eBook/ebookb5/db/models/MailingList.java
+++ src/main/java/kg/eBook/ebookb5/db/models/MailingList.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Table
 @Entity
@@ -24,4 +26,5 @@ public class MailingList {
     public MailingList(String email) {
         this.email = email;
     }
+
 }

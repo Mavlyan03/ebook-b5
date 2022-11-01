6b81e3d56a2b95a5b1fa694a7d2fd79d85b690f1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 16:57:43 +0600

    refactor SearchType

--- src/main/java/kg/eBook/ebookb5/enums/SearchType.java
+++ src/main/java/kg/eBook/ebookb5/enums/SearchType.java
@@ -1,18 +1,13 @@
 package kg.eBook.ebookb5.enums;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public enum SearchType {
 
     GENRE,
-
     BOOK,
-
     AUTHOR,
-
-    PUBLISHING_HOUSE;
-
+    PUBLISHING_HOUSE
 
 }

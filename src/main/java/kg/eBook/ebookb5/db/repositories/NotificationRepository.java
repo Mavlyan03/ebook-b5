9ff9c30a268102a616e40d3213135c192d4df67b
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 15:26:48 +0600

    refactor NotificationRepository

--- src/main/java/kg/eBook/ebookb5/db/repositories/NotificationRepository.java
+++ src/main/java/kg/eBook/ebookb5/db/repositories/NotificationRepository.java
@@ -7,16 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-    Optional<Notification> findByBookId(Long bookId);
 
     @Query("select new kg.eBook.ebookb5.dto.responses.NotificationResponse " +
             "(n.id, n.bookStatus, n.createdAt, n.read ) from Notification n " +
             "where n.vendor.id=:vendorId " +
             "order by n.read asc ")
     List<NotificationResponse> findAllNotifications(Long vendorId);
+
 }

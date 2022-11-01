c162b1ceddeecbf1d1a6d19374e8d419696aa45e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 17:17:26 +0600

    refactor InvalidDateException

--- src/main/java/kg/eBook/ebookb5/exceptions/InvalidDateException.java
+++ src/main/java/kg/eBook/ebookb5/exceptions/InvalidDateException.java
@@ -1,10 +1,12 @@
 package kg.eBook.ebookb5.exceptions;
 
-public class InvalidDateException extends RuntimeException{
+public class InvalidDateException extends RuntimeException {
+
     public InvalidDateException() {
     }
 
     public InvalidDateException(String message) {
         super(message);
     }
+
 }

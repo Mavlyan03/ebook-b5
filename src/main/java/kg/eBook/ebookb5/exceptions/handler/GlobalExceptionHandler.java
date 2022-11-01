f606d8293ab4913fd0928f937435a9982d3adacc
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 17:16:05 +0600

    refactor GlobalExceptionHandler

--- src/main/java/kg/eBook/ebookb5/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/kg/eBook/ebookb5/exceptions/handler/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package kg.eBook.ebookb5.exceptions.handler;
 
-import kg.eBook.ebookb5.exceptions.*;
+import kg.eBook.ebookb5.exceptions.AlreadyExistException;
+import kg.eBook.ebookb5.exceptions.ExceptionResponse;
+import kg.eBook.ebookb5.exceptions.InvalidDateException;
+import kg.eBook.ebookb5.exceptions.InvalidPromocodeException;
+import kg.eBook.ebookb5.exceptions.NotFoundException;
+import kg.eBook.ebookb5.exceptions.WrongEmailException;
+import kg.eBook.ebookb5.exceptions.WrongPasswordException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -82,4 +90,5 @@ public class GlobalExceptionHandler {
                 e.getMessage()
         );
     }
+
 }

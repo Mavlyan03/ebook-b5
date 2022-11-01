7e2d132a619302262b2d0477c1946c993edf64a7
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 12:51:58 +0600

    refactor MailingApi

--- src/main/java/kg/eBook/ebookb5/apis/MailingApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/MailingApi.java
@@ -1,35 +1,34 @@
 package kg.eBook.ebookb5.apis;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.eBook.ebookb5.dto.requests.RequestMailingList;
 import kg.eBook.ebookb5.services.MailingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/mailing")
 @RequiredArgsConstructor
+@RequestMapping("api/mailing")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Mailing API", description = "Mailing endpoints")
 public class MailingApi {
 
     private final MailingService emailService;
 
-    @PostMapping("/signup")
+    @Operation(summary = "Send mailing", description = "Подписка на рассылку")
     @PreAuthorize("permitAll()")
-    @Operation(description = "Подписка на рассылку")
+    @PostMapping("signup")
     public ResponseEntity<String> sendMail(@RequestBody RequestMailingList requestMailingList) {
         emailService.sendSignUpMessage(requestMailingList);
         return ResponseEntity.ok("Письмо отправлено");
     }
-
-//    @PostMapping("/send-to-all-emails")
-//    @PreAuthorize("hasAnyAuthority('VENDOR', 'USER', 'ADMIN')")
-//    @Operation(description = "Отправка почты всем подписанным пользователям о новой книге")
-//    public ResponseEntity<String> sendToAllEmails(@RequestBody ) {
-//        emailService.sendNewBookMessage();
-//        return ResponseEntity.ok("Почта о добавлении новой книги отправлено пользователям");
-//    }
 
 }

7ffb6ecae051827040a0ebf0f6b95d78271c28fd
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 12:59:31 +0600

    refactor PromocodeApi

--- src/main/java/kg/eBook/ebookb5/apis/PromocodeApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/PromocodeApi.java
@@ -9,6 +9,7 @@ import kg.eBook.ebookb5.services.PromocodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,24 +24,23 @@ import java.util.List;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("api/promocode")
-@Tag(name = "Promocode API", description = "Vendor can create promo code, and 'user check that promo code")
+@Tag(name = "Promocode API", description = "Promo code endpoints for Vendor & User")
 public class PromocodeApi {
 
     private final PromocodeService promocodeService;
 
-    @PreAuthorize("hasAuthority('VENDOR')")
     @Operation(summary = "Create promo code", description = "Vendor can create promo code")
+    @PreAuthorize("hasAuthority('VENDOR')")
     @PostMapping
-    public SimpleResponse create(@RequestBody PromocodeRequest promoCodeRequest,
-                                 Authentication authentication) {
+    public SimpleResponse create(@RequestBody PromocodeRequest promoCodeRequest, Authentication authentication) {
         return promocodeService.createPromoCode(promoCodeRequest, authentication);
     }
 
+    @Operation(summary = "Get all books by promo code", description = "Get all books by promo code for user")
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "User can check promo code with name", description = "User if the promo code is valid, it will return discounted books")
     @GetMapping("/check")
-    public List<BookBasketResponse> findAllBooksWithPromocode(@RequestParam String promocodeName,
-                                                              Authentication authentication) {
+    public List<BookBasketResponse> findAllBooksWithPromocode(@RequestParam String promocodeName, Authentication authentication) {
         return promocodeService.findAllBooksWithPromocode(promocodeName, authentication);
     }
+
 }

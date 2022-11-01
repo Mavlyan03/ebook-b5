a502be3e076fcc68fcccad6fd908390648d00bd5
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-01 13:37:36 +0600

    refactor UserCartApi

--- src/main/java/kg/eBook/ebookb5/apis/UserCartApi.java
+++ src/main/java/kg/eBook/ebookb5/apis/UserCartApi.java
@@ -1,6 +1,7 @@
 package kg.eBook.ebookb5.apis;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.eBook.ebookb5.dto.responses.BookBasketResponse;
 import kg.eBook.ebookb5.services.PromocodeService;
 import lombok.RequiredArgsConstructor;
@@ -8,55 +9,66 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/cart")
+@RequestMapping("api/cart")
 @PreAuthorize("hasAuthority('USER')")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Cart API", description = "Cart endpoints for user")
 public class UserCartApi {
 
     private final PromocodeService promocodeService;
 
-    @GetMapping("/check")
-    @Operation(summary = "get all books in users cart, check promo code with name", description = "if the promo code is valid, it will return discounted books")
+    @Operation(summary = "Get all books in users cart", description = "If the promo code is valid, it will return discounted books")
+    @GetMapping("check")
     public List<BookBasketResponse> findAllBooksWithPromocode(
             @RequestParam(required = false) String promocodeName, Authentication authentication) {
         return promocodeService.findAllBooksWithPromocode(promocodeName, authentication);
     }
 
-    @PostMapping("/plus-button/{bookId}")
+    @Operation(summary = "Increase book quantity", description = "Increase book quantity in cart by user")
+    @PostMapping("plus-button/{bookId}")
     public int plusButton(@PathVariable Long bookId, @RequestParam Integer plus) {
         return promocodeService.increaseBooksToBuy(bookId, plus);
     }
 
-    @PostMapping("/minus-button/{bookId}")
+    @Operation(summary = "Decrease book quantity", description = "Decrease book quantity in cart by user")
+    @PostMapping("minus-button/{bookId}")
     public int minusButton(@PathVariable Long bookId, @RequestParam Integer minus, Authentication authentication) {
         return promocodeService.decreaseBookToBuy(bookId, minus, authentication);
     }
 
+    @Operation(summary = "Add book to cart", description = "Adding book to cart by user")
     @PostMapping("/{bookId}")
-    @Operation(summary = "Adding book to cart")
     public ResponseEntity<HttpStatus> addBookToBasketList(@PathVariable Long bookId, Authentication authentication) {
         promocodeService.addBookToBasketList(bookId, authentication);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete book from cart", description = "Removing book from cart by user")
     @PutMapping("/{bookId}")
-    @Operation(summary = "Remove book to cart")
     public ResponseEntity<HttpStatus> removeBookToBasketList(@PathVariable Long bookId, Authentication authentication) {
         promocodeService.removeBookToBasketList(bookId, authentication);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete all book from cart", description = "Removing all books from cart by user")
     @PutMapping
-    @Operation(summary = "Remove all book to cart")
     public ResponseEntity<HttpStatus> removeAllBooksToBasketList(Authentication authentication) {
         promocodeService.removeAllBooksToBasketList(authentication);
         return ResponseEntity.ok(HttpStatus.OK);
     }
+
 }
